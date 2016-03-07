eb22bf4f712deb9cbd196a0cacdbaf4081b83341
FilipaRamos <fimamora@hotmail.com>
2016-03-07 21:16:26 +0000

    lab II missing only status response from server

--- Lab II/MulticastPlatesNames/src/ServerClient/Client.java
+++ Lab II/MulticastPlatesNames/src/ServerClient/Client.java
@@ -1,10 +1,135 @@
 package ServerClient;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
 public class Client {
-	public static void main (String[] args) {
-		if(args.length != 4){
+	
+	// multicast data read from the input
+	public int mcast_port;
+	public String mcast_addr;
+	public String operator;
+	
+	// socket data gotten from the multicast communication
+	String srvc_addr;
+	int srvc_port;
+
+	// constructor
+	public Client() {}
+
+	public static void main(String[] args) {
+		if (args.length < 4 || args.length > 5) {
 			System.out.println("Wrong number of arguments!");
 		}
-		
+
+		// create a client
+		Client client = new Client();
+
+		// save the arguments
+		client.mcast_addr = args[0];
+		client.mcast_port = Integer.parseInt(args[1]);
+		client.operator = args[2];
+
+		// create the multicast socket
+		MulticastSocket multicastSocket;
+
+		try {
+			///////////////////////////////////////////////////////////////////
+			/////////////////////////// MULTICAST /////////////////////////////
+			
+			// settle the multicast IP address
+			InetAddress Multicast_IP_address = InetAddress.getByName(client.mcast_addr);
+			
+			// join the group
+			multicastSocket = new MulticastSocket(client.mcast_port);
+			multicastSocket.joinGroup(Multicast_IP_address);
+			System.out.println("Joined Multicast group!");
+
+			// to save the received message over multicast
+			byte buf[] = new byte[256];
+			DatagramPacket advertisement = new DatagramPacket(buf, buf.length, Multicast_IP_address, client.mcast_port);
+			System.out.println("Received message");
+			multicastSocket.receive(advertisement);
+			System.out.println("Received multicast message");
+			
+			///////////////////////////////////////////////////////////////////
+
+			// split the string
+			String data = new String(advertisement.getData()).substring(0, advertisement.getLength());
+			String[] adv_split = data.split(" ");
+			
+			// get the port and the address of the socket
+			client.srvc_port = Integer.parseInt(adv_split[0]);
+			client.srvc_addr = adv_split[1];
+			
+			System.out.println(client.srvc_port);
+			System.out.println(client.srvc_addr);
+			
+			multicastSocket.leaveGroup(Multicast_IP_address);
+
+			// print the log
+			client.log();
+
+			// create the normal socket
+			DatagramSocket socket = new DatagramSocket();
+			InetAddress IP_address = InetAddress.getByName(client.srvc_addr);
+
+			// store the request
+			String toSend = null;
+			byte[] sendRequest = new byte[256];
+			
+			// store the response
+			String toReceive = null;
+			byte[] response = new byte[256];
+
+			// process request
+			if (args[2].equals("register")) {
+				if (args.length != 5) {
+					System.out.println("Wrong number of arguments for command register.");
+					System.exit(0);
+				}
+				toSend = "register " + args[3] + " " + args[4] + "\n";
+			} else if (args[2].equals("lookup")) {
+				if (args.length != 4) {
+					System.out.println("Wrong number of arguments for command register.");
+					System.exit(0);
+				}
+				toSend = "lookup " + args[3] + "\n";
+			}
+
+			// send request to server through the socket
+			sendRequest = toSend.getBytes();
+			DatagramPacket sendPacket = new DatagramPacket(sendRequest, sendRequest.length, IP_address,
+					client.srvc_port);
+			socket.send(sendPacket);
+			System.out.println("Awaiting status from server...");
+
+			// receive the status from the server
+			DatagramPacket receivePacket = new DatagramPacket(response, response.length);
+
+			System.out.println("received |||||||||");
+			socket.receive(receivePacket);
+			System.out.println("received");
+			// received information
+			toReceive = new String(receivePacket.getData());
+			System.out.println("Status from server:");
+			
+			// close the socket
+			socket.close();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	// print the log
+	public void log() {
+		String log = "multicast: " + mcast_addr + " " + mcast_port + " : " + srvc_addr + " " + srvc_port;
+		System.out.println(log);
 	}
 }
--- Lab II/MulticastPlatesNames/src/ServerClient/Server.java
+++ Lab II/MulticastPlatesNames/src/ServerClient/Server.java
@@ -4,9 +4,10 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.MulticastSocket;
 import java.util.ArrayList;
 
-public class Server {
+public class Server implements Runnable {
 	// database
 	public ArrayList<String> database = new ArrayList<String>();
 	// port number in which the server provides the service
@@ -15,132 +16,170 @@ public class Server {
 	public static String mcast_addr;
 	// multicast group port number
 	public static int mcast_port;
+	// the Socket to be used
+	public static DatagramSocket socket = null;
 	// the multicastSocket to be used
-	public DatagramSocket socket = null;
+	public static MulticastSocket multicastSocket = null;
+	//
+	public static byte[] bufferMulticast = new byte[256];
+	//
+	public static InetAddress Multicast_IP_address;
+	//
+	public static InetAddress IP_address;
+	//
+	public static String advertisement;
 
 	// Class constructor
-	public Server() {
+	public Server() throws IOException {
+		multicastSocket = new MulticastSocket(mcast_port);
+		System.out.println("Launching Multicast Server Thread!");
 	}
 
 	public static void main(String[] args) throws IOException {
 		if (args.length != 3) {
 			System.out.println("Wrong number of arguments!");
 		}
+		
+		// store the messages
+		byte[] bufferReceived = new byte[256];
+
+		// settle the IP_address
+		IP_address = InetAddress.getLocalHost();
+
+		/*
+		 * Multicast Thread
+		 */
 
 		srvc_port = Integer.parseInt(args[0]);
-		System.out.println(srvc_port);
 		mcast_addr = args[1];
-		System.out.println(mcast_addr);
 		mcast_port = Integer.parseInt(args[2]);
-		System.out.println(mcast_port);
+
+		// settle the IP address
+		Multicast_IP_address = InetAddress.getByName(mcast_addr);
+
+		// message to advertise the IP address and port number
+		String localhost = InetAddress.getLocalHost().getHostAddress();
+		advertisement = srvc_port + " " + localhost;
 
 		Server server = new Server();
-		server.start();
-		server.serverEngine(server);
+
+		new Thread(server).start();
+
+		server.serverEngine(server, bufferReceived);
 
 	}
 
-	// Create the DatagramSocket
-	public void start() throws IOException {
-		socket = new DatagramSocket(srvc_port);
-		System.out.println("Launching Multicast Server Thread!");
-		// set the socket timeout to 1 s so that it doesn't wait too long for a message to be received
-		socket.setSoTimeout(1000);
-	}
+	public void serverEngine(Server server, byte[] bufferReceived) {
 
-	@SuppressWarnings("null")
-	public void serverEngine(Server server) throws IOException{
+		while (true) {
 
-		while(true){
-			
-			// settle the IP_address
-			InetAddress IP_address = InetAddress.getByName(mcast_addr);
-			
-			// store the messages
-			byte[] bufferReceived = new byte[256];
-			byte[] bufferSend = new byte[256];
-			
-			// message to advertise the IP address and port number
-			String advertisement = "Server offers service in " + mcast_addr + srvc_port;
-			
-			// message to be sent
-			String message = null;
-			
-			try{	
-				// send the advertisement
-				bufferSend = advertisement.getBytes();
-				DatagramPacket toSend = new DatagramPacket(bufferSend, bufferSend.length, IP_address, srvc_port);
-				socket.send(toSend);
+			try {
+				// message to be sent
+				String status = null;
+				String request = null;
 				
+				// initiate the socket
+				socket = new DatagramSocket(srvc_port);
+
 				// get the client's request
-				DatagramPacket receivedPacket = new DatagramPacket(bufferReceived, bufferReceived.length, IP_address, srvc_port);
+				DatagramPacket receivedPacket = new DatagramPacket(bufferReceived, bufferReceived.length);
 				socket.receive(receivedPacket);
-				String request = new String(receivedPacket.getData());
-				System.out.println("Server received: " + request);
-				
-				// if a request was received
-				if (request != null) {
-					
-					// perform the requested operation
-					String[] splitted = request.split(" ");
-					// register
-					if (splitted[0].equals("register")) {
-						System.out.println("Register operation requested. Plate number: " + splitted[1]
-								+ ". Owner name: " + splitted[2]);
-						server.register(splitted[1], splitted[2]);
-						message = "register " + splitted[1] + " " + splitted[2];
-					}
-					// lookup
-					if (splitted[0].equals("lookup")) {
-						System.out.println("Lookup operation requested. Plate number: " + splitted[1]);
-						String owner_name = server.lookup(splitted[1]);
-						message = "lookup " + splitted[1] + " " + owner_name;
-					} else { // error
-						message = "error";
-					}
+				request = new String(receivedPacket.getData());
+				System.out.println("Server received the request: ");
+				System.out.println(request);
+
+				// perform the requested operation
+				String[] splitted = request.split(" ");
+
+				// register
+				if (splitted[0].equals("register")) {
+					System.out.println("Register operation requested. Plate number: " + splitted[1] + ". Owner name: "
+							+ splitted[2]);
+					server.register(splitted[1], splitted[2]);
+					status = "register " + splitted[1] + " " + splitted[2] + "\n";
 				}
-			}
-			catch(IOException X){
+				// lookup
+				else if (splitted[0].equals("lookup")) {
+					System.out.println("Lookup operation requested. Plate number: " + splitted[1]);
+					String owner_name = server.lookup(splitted[1]);
+					status = "lookup " + splitted[1] + " " + owner_name + "\n";
+				} else { // error
+					status = "error" + "\n";
+				}
+				
+				// send the status
+				System.out.println(receivedPacket.getAddress());
+				System.out.println(srvc_port);
+				DatagramPacket sendPacket = new DatagramPacket(status.getBytes(), status.getBytes().length, receivedPacket.getAddress(), srvc_port);
+				socket.send(sendPacket);
+				System.out.println("Server finalized!");
+				
+				socket.close();
+
+			} catch (IOException X) {
 				X.printStackTrace();
 				break;
 			}
-			
 		}
 	}
-	
-	public void log(){ 
-		String log = "multicast: " + 
-				mcast_addr + mcast_port + 
-				":" + srvc_port;
+
+	// print the log
+	public void log() {
+		String log = "multicast: " + mcast_addr + " " + mcast_port + " : " + srvc_port;
 		System.out.print(log);
 	}
-	
+
 	// add the plate to the database if it doesn't exist yet
-	public void register(String plate_number, String owner_name){
+	public void register(String plate_number, String owner_name) {
 		System.out.println("Adding register to the database...");
 
 		String newPlate = plate_number + " " + owner_name;
-		if(!database.contains(newPlate))
+		if (!database.contains(newPlate))
 			database.add(newPlate);
-		
+
 		System.out.println("Plate added successfully!");
 	}
-	
+
 	// find the plate and return the owner_name
-	public String lookup(String plate_number){
+	public String lookup(String plate_number) {
 		System.out.println("Looking for plate number...");
-		
+
 		String owner_name = null;
-		
-		for(int i = 0; i < database.size(); i++){
+
+		for (int i = 0; i < database.size(); i++) {
 			String register = database.get(i);
 			String[] splitted = register.split(" ");
-			if(splitted[0].equals(plate_number)){
+			if (splitted[0].equals(plate_number)) {
 				owner_name = splitted[1];
 			}
 		}
-		
+
 		System.out.println("Plate found successfully!");
 		return owner_name;
+	}
+
+	@Override
+	public void run() {
+		try {
+			// set time to live
+			multicastSocket.setTimeToLive(1);
+			while (true) {
+				// send the advertisement
+				bufferMulticast = advertisement.getBytes();
+				System.out.println(advertisement);
+
+				DatagramPacket toSend = new DatagramPacket(bufferMulticast, bufferMulticast.length,
+						Multicast_IP_address, mcast_port);
+				multicastSocket.send(toSend);
+
+				System.out.println("sent multicast");
+				Thread.sleep(1000);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
