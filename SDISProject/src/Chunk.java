e18f0b8c45877c477954b942741103b85cdf7d9a
FilipaRamos <fimamora@hotmail.com>
2016-03-10 17:27:44 +0000

    thread engine ready

--- SDISProject/src/Chunk.java
+++ SDISProject/src/Chunk.java
@@ -1,19 +1,47 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Chunk {
 	// the id of the file to which the chunk belongs to
-	public int fileId;
+	public byte[] fileId;
+	// the name of the file
+	public String filename;
 	// the number of the chunk
 	public int chunkNo;
-	// size of the chunk
-	public int chunkSize;
+	// the data of the chunk
+	public byte[] chunkData;
 	
 	// constructor
-	public Chunk(int fileId, int chunkNo, int chunkSize){
+	public Chunk(byte[] fileId, String filename, int chunkNo, byte[] chunkData){
 		
 		this.fileId = fileId;
+		this.filename = filename;
 		this.chunkNo = chunkNo;
-		this.chunkSize = chunkSize;
+		this.chunkData = chunkData;
 		
+	}
+	
+	// to write the chunk in the disk
+	public void WriteChunk(Chunk chunk) {
+		String outputFile = chunk.filename + chunk.chunkNo;
+
+		File dstFile = new File(outputFile);
+		FileOutputStream fileOutputStream = null;
+		try {
+			
+			fileOutputStream = new FileOutputStream(dstFile);
+			fileOutputStream.write(chunk.chunkData);
+			fileOutputStream.flush();
+			fileOutputStream.close();
+			
+			System.out.println("Output file : " + outputFile + " is successfully saved! ");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- SDISProject/src/ServerFile.java
+++ SDISProject/src/ServerFile.java
@@ -1,4 +1,8 @@
 import java.util.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class ServerFile {
 	// the id of the server to which the original copy of the file belongs to
@@ -6,7 +10,7 @@ public class ServerFile {
 	// the name of the file
 	public String name;
 	// the identifier of the file
-	public String identifier;
+	public byte[] identifier;
 	// Name of the owner of the file
 	public String owner;
 	// When the file was modified
@@ -26,40 +30,54 @@ public class ServerFile {
 		this.size = size;
 		this.owner = owner;
 		this.chunksNo = this.size / 64000 + 1;
-		
+
+		// create the identifier
+		try {
+			String fileId = Integer.toString(this.homeServer) + this.name + this.owner;
+			this.identifier = hash(fileId);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+
 		System.out.println("File belongs to server: " + this.homeServer);
 		System.out.println("Filename: " + this.name);
 		System.out.println("File size: " + this.size);
 		System.out.println("Owner of the file: " + this.owner);
 		System.out.println("Number of chunks to be used: " + this.chunksNo);
-		
-		int missedSize = this.size;
-		int filledSize = 0;
-		
-		for(int i = 0; i < this.chunksNo; i++){
-			if(missedSize >= 64000){
-				
-				filledSize = 64000;
-				missedSize = missedSize - filledSize;
-				
-				System.out.println("Chunk Size --> " + filledSize);
-				Chunk chunk = new Chunk(1, i, filledSize);
+
+	}
+
+	// splits the file in chunks
+	public void splitFile(File f) throws FileNotFoundException, IOException {
+
+		int partCounter = 0;
+
+		// maximum size of chunks is 64 kb
+		int chunksSize = 64 * 1024;
+
+		byte[] buffer = new byte[chunksSize];
+
+		try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(f))) {
+
+			String name = f.getName();
+
+			int tmp = 0;
+			while ((tmp = bis.read(buffer)) > 0) {
+				// write each chunk of data into separate file with different
+				Chunk chunk = new Chunk(identifier, name, partCounter, buffer);
 				chunks.add(chunk);
-				continue;
-				
-			}
-			else if(missedSize < 64000){
-				
-				filledSize = missedSize;
-				
-				System.out.println("Chunk Size --> " + filledSize);
-				Chunk chunk = new Chunk(1, i, filledSize);
-				chunks.add(chunk);
-				continue;
-				
+				System.out.println("Chunk created -> nr " + partCounter);
 			}
 		}
+	}
 
+	// hash the string (to create file identifier)
+	public byte[] hash(String text) throws NoSuchAlgorithmException {
+
+		MessageDigest digest = MessageDigest.getInstance("SHA-256");
+		byte[] hash = digest.digest(text.getBytes(StandardCharsets.UTF_8));
+
+		return hash;
 	}
 
 }
